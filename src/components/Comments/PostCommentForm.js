c3ed0f241c3c7f567bdd1f0548d27ec6b6855112
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-02-16 21:21:36 +0700

    [feature][like_idea] like and unlike idea

--- src/components/Comments/PostCommentForm.js
+++ src/components/Comments/PostCommentForm.js
@@ -1,57 +1,61 @@
-import React, { useState } from "react";
-import {
-  Box,
-  FormControl,
-  FormLabel,
-  Textarea,
-  Button,
-} from "@chakra-ui/react";
+import React, {useState} from "react";
+import {Box, Button, FormControl, FormLabel, Textarea, useToast} from "@chakra-ui/react";
 import Api from "../../api/Api";
-import { useDispatch } from "react-redux";
-import { getCommentOfIdea } from "../../app/reducers/ideasSlice";
+import {useDispatch} from "react-redux";
+import {getCommentOfIdea} from "../../app/reducers/ideasSlice";
 
-const PostCommentForm = ({ ideaId }) => {
-  const [textComment, setTextComment] = useState("");
-  const dispatch = useDispatch();
+const PostCommentForm = ({ideaId}) => {
+    const [textComment, setTextComment] = useState("");
+    const dispatch = useDispatch();
+    const toast = useToast()
+    const handlePostComment = async () => {
+        if (textComment === "") {
+            alert("Please input content comment");
+        } else {
+            try {
+                let formData = new FormData();
+                formData.append("content", textComment);
+                await Api().post(`ideas/${ideaId}/comments`, formData);
+                dispatch(getCommentOfIdea(ideaId));
+                toast({
+                    status: 'success',
+                    title: 'Post comment success',
+                    duration: 9000,
+                    position: 'top-right',
+                    isClosable: true,
+                })
 
-  const handlePostComment = async () => {
-    if (textComment === "") {
-      alert("Please input content comment");
-    } else {
-      try {
-        let formData = new FormData();
-        formData.append("content", textComment);
-        await Api().post(`ideas/${ideaId}/comments`, formData);
-        dispatch(getCommentOfIdea(ideaId));
-        alert("Post comment success");
-        setTextComment("");
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
-  return (
-    <>
-      <Box p="5" m={"5"} borderWidth="1px" borderRadius="lg">
-        <FormControl>
-          <FormLabel htmlFor="email">Enter your comment</FormLabel>
-          <Textarea
-            value={textComment}
-            onChange={(e) => setTextComment(e.target.value)}
-            placeholder="Write your comment"
-          ></Textarea>
-          <Button
-            onClick={handlePostComment}
-            align={"left"}
-            mt={4}
-            colorScheme="teal"
-          >
-            Post comment
-          </Button>
-        </FormControl>
-      </Box>
-    </>
-  );
+                setTextComment("");
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
+
+    return (
+        <>
+            <Box p="5" m={"5"} borderWidth="1px" borderRadius="lg">
+                <FormControl>
+                    <FormLabel htmlFor="email">Enter your comment</FormLabel>
+                    <Textarea
+                        value={textComment}
+                        onChange={(e) => setTextComment(e.target.value)}
+                        placeholder="Write your comment"
+                    />
+                    <Button
+                        onClick={handlePostComment}
+                        align={"left"}
+                        mt={4}
+                        colorScheme="teal"
+                    >
+                        Post comment
+                    </Button>
+                </FormControl>
+            </Box>
+        </>
+    );
 };
 
+
 export default PostCommentForm;
+
