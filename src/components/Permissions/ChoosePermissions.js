1d33c08d4ef00e42c757db08d33dac02930d51ff
Tran Ngoc Phuoc <phuoc04012000@gmail.com>
2022-03-01 22:22:49 +0700

    [feature][qa_cd_manager_staff_permissions]

--- src/components/Permissions/ChoosePermissions.js
+++ src/components/Permissions/ChoosePermissions.js
@@ -1,20 +1,13 @@
 import {Button, Select} from "@chakra-ui/react";
 import {useState} from "react";
 
-const ChoosePermissions = ({permissions}) => {
-    const [listPermissionId, setListPermissionId] = useState([])
 
-    const changeListPermission = (e) => {
-        let value = Array.from(e.target.selectedOptions, option => option.value);
-        setListPermissionId([...value])
-        console.log(listPermissionId)
-    }
+const ChoosePermissions = ({permissions, handleUpdatePermissionsFunc}) => {
+
+
 
     return (<>
-        <Select onChange={changeListPermission} multiple>
-            {permissions && permissions.map(e => <option key={e.id} value={e.id}>{e.name}</option>)}
-        </Select>
-        <Button>Update permission</Button>
+
     </>)
 }
 export default ChoosePermissions
